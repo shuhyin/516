701945e588998f653087b9fe8c9a69db07834914
shuhyin <89762819+shuhyin@users.noreply.github.com>
2021-11-29 10:23:19 -0600

    Update project3.py

--- project3.py
+++ project3.py
@@ -1,3 +1,6 @@
+
+#convert 2 patterns of British English to American English 
+
 #open the input file 
 with open('hello.txt') as hello_doc:
     hello_content = hello_doc.read()
@@ -7,7 +10,7 @@ import re
 hello_new1 = re.sub(r"\b(\w{2,})our\b", r"\1or", hello_content) # change words ending with 'our' to 'or'
 # print(hello_new1)
 
-hello_new2 = re.sub(r"\b(\w+[aeiou])ll(\w+)\b", r"\1l\2", hello_new1) #convert doubled spelling ‘L’ in verbs ending in a vowel plus ‘L’.double l 'll' to single 'l'
+hello_new2 = re.sub(r"\b(\w+[aeiou])ll(\w+)\b", r"\1l\2", hello_new1) #for doubled spelling ‘L’ in verbs ending in a vowel plus ‘L’, convert it to single 'l'
 print(hello_new2)
 
 #write the output file 
